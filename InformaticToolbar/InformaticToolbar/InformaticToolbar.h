1b29c73f41cd0705d3e732d9862c7711921083a1
Greg Wang <gregwym@qq.com>
2012-10-14 01:55:52 -0400

    Add facade header

---
+++ InformaticToolbar/InformaticToolbar/InformaticToolbar.h
@@ -0,0 +1,17 @@
+//
+//  InformaticToolbar.h
+//  InformaticToolbar
+//
+//  Created by Greg Wang on 12-10-14.
+//  Copyright (c) 2012年 Greg Wang. All rights reserved.
+//
+
+#ifndef InformaticToolbar_InformaticToolbar_h
+#define InformaticToolbar_InformaticToolbar_h
+
+#import "UIViewController+InformaticToolbar.h"
+#import "ITBarItemSet.h"
+#import "ITLabelBarItemSet.h"
+#import "ITProgressBarItemSet.h"
+
+#endif
